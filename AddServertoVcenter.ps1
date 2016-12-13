760e6a7eec1cb843568f7e917a171698bcc81c14
ephermal <gerald.reid@gmail.com>
2016-12-13 12:44:01 -0600

    Create AddServertoVcenter.ps1

---
+++ AddServertoVcenter.ps1
@@ -0,0 +1,14 @@
+-#Script to Add Hosts to Vcenter; Used for when you have ### Machines but wait 6 years to get vCenter :-)		
+-#This script was created by Jerry Reid		
+-#It is a simple loop just adjust the numbers and use the hostname		
+-		
+-Add-PSSnapin VMware.VimAutomation.Core 		
+-		
+-Connect-ViServer $VCENTERHOST -Username Administrator@vsphere.local -Password $PASSWORD		
+-		
+-		
+- foreach ($i in 10..29) 		
+-{		
+-Foreach-Object { Add-VMHost $HOSTNAME"$i".FQDN -Location (Get-Datacenter -Name $DATACENTER_NAME) -User root -Password $PASSWORD -RunAsync -force:$true}		
+-}		
+-
